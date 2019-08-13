6420d81b94236bf3ea08dfb986e5d3d6695ff1fb
HuuTho99 <myworld20011999@gmail.com>
2019-08-13 21:39:42 +0700

    1

--- khang/models/sanpham.js
+++ khang/models/sanpham.js
@@ -4,7 +4,8 @@ var Schema = mongoose.Schema;
 var SanPhamSchema = new Schema({
     _id: mongoose.Schema.Types.ObjectId,
     jobCode: String,
-    baoGia: [{type: mongoose.Schema.Types.ObjectId, ref: 'bao_gia'}]
+    baoGia: [{type: mongoose.Schema.Types.ObjectId, ref: 'bao_gia'}],
+    yeuCauSanXuat: {type: mongoose.Schema.Types.ObjectId, ref: 'yeu_cau_san_xuat'}
 },{collection: 'san_pham'});
 
 module.exports = mongoose.model('san_pham', SanPhamSchema);
--- khang/routes/index.js
+++ khang/routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var sanPhamModel = require('../models/sanpham');
+var mongoose = require('mongoose');
 var router = express.Router();
 
 
@@ -7,9 +8,22 @@ var router = express.Router();
 /* GET home page. */
 router.get('/',async function(req, res, next) {
   var data = await sanPhamModel.find({});
-  console.log(data);
+  // console.log(data);
   
   res.render('index', { title: 'Express', data: data });
 });
+
+router.post('/them-job',(req, res, next) =>{
+  const jobCode = new sanPhamModel({
+    _id: new mongoose.Types.ObjectId(),
+    jobCode: req.body.jobCode
+  })
+  console.log(jobCode);
+  
+  jobCode.save();
+
+  res.redirect('/');
+
+})
 
 module.exports = router;
